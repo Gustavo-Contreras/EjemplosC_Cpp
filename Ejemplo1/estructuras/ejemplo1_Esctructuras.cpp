6cb16703406a9ef2ef255f3c44181c9c8fb4b739
Gustavo-Contreras <50632467+Gustavo-Contreras@users.noreply.github.com>
2019-06-18 11:38:45 -0500

    ejemplo 1 de estrcuturas
    cuerpo de una estructura

---
+++ Ejemplo1/estructuras/ejemplo1_Esctructuras.cpp
@@ -0,0 +1,26 @@
+/* creee una estructura para almacenar clientes.
+*los clientes tienes
+* un noombre maximo 20 caracteres
+*el numero solicitado de unidades
+*precio
+*Estado
+*/
+
+#include <iostream>
+
+using namespace std;
+struct Cliente
+{
+	char nombre [20];
+	int numUnidades;
+	float precio;
+	char estado;
+};
+
+int main ()
+{
+	
+	
+	
+	return 0;
+}
