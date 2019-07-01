31d5cb4dbb306d3b2f446dd013c20e6cc68ca338
Gustavo-Contreras <50632467+Gustavo-Contreras@users.noreply.github.com>
2019-07-01 11:09:06 -0500

    INTRODUCCION ARCHIVOS
    llamar directorios y utilizar herramientas de la interfaz de windows

--- Ejemplo1/Archivos/Introduccion Archivos/Introduccion Archivos/Form1.Designer.cs
+++ Ejemplo1/Archivos/Introduccion Archivos/Introduccion Archivos/Form1.Designer.cs
@@ -28,7 +28,65 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.dgvLogs = new System.Windows.Forms.DataGridView();
+            this.tipo = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.log = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.btnAñadirLog = new System.Windows.Forms.Button();
+            this.btnLeerDirectorio = new System.Windows.Forms.Button();
+            this.txbLeerDirectorio = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvLogs)).BeginInit();
             this.SuspendLayout();
+            // 
+            // dgvLogs
+            // 
+            this.dgvLogs.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvLogs.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.tipo,
+            this.log});
+            this.dgvLogs.Location = new System.Drawing.Point(106, 136);
+            this.dgvLogs.Name = "dgvLogs";
+            this.dgvLogs.Size = new System.Drawing.Size(444, 150);
+            this.dgvLogs.TabIndex = 0;
+            this.dgvLogs.CellDoubleClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvLogs_CellDoubleClick);
+            // 
+            // tipo
+            // 
+            this.tipo.HeaderText = "Tipo Log";
+            this.tipo.Name = "tipo";
+            this.tipo.ReadOnly = true;
+            // 
+            // log
+            // 
+            this.log.HeaderText = "Descripcion ";
+            this.log.Name = "log";
+            this.log.ReadOnly = true;
+            // 
+            // btnAñadirLog
+            // 
+            this.btnAñadirLog.Location = new System.Drawing.Point(106, 83);
+            this.btnAñadirLog.Name = "btnAñadirLog";
+            this.btnAñadirLog.Size = new System.Drawing.Size(200, 23);
+            this.btnAñadirLog.TabIndex = 1;
+            this.btnAñadirLog.Text = "Añadir log";
+            this.btnAñadirLog.UseVisualStyleBackColor = true;
+            this.btnAñadirLog.Click += new System.EventHandler(this.btnAñadirLog_Click);
+            // 
+            // btnLeerDirectorio
+            // 
+            this.btnLeerDirectorio.Location = new System.Drawing.Point(343, 83);
+            this.btnLeerDirectorio.Name = "btnLeerDirectorio";
+            this.btnLeerDirectorio.Size = new System.Drawing.Size(207, 23);
+            this.btnLeerDirectorio.TabIndex = 2;
+            this.btnLeerDirectorio.Text = "Leer Directorio";
+            this.btnLeerDirectorio.UseVisualStyleBackColor = true;
+            this.btnLeerDirectorio.Click += new System.EventHandler(this.btnLeerDirectorio_Click);
+            // 
+            // txbLeerDirectorio
+            // 
+            this.txbLeerDirectorio.Location = new System.Drawing.Point(106, 35);
+            this.txbLeerDirectorio.Name = "txbLeerDirectorio";
+            this.txbLeerDirectorio.Size = new System.Drawing.Size(444, 20);
+            this.txbLeerDirectorio.TabIndex = 3;
             // 
             // Form1
             // 
@@ -36,15 +94,28 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txbLeerDirectorio);
+            this.Controls.Add(this.btnLeerDirectorio);
+            this.Controls.Add(this.btnAñadirLog);
+            this.Controls.Add(this.dgvLogs);
             this.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvLogs)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.DataGridView dgvLogs;
+        private System.Windows.Forms.DataGridViewTextBoxColumn tipo;
+        private System.Windows.Forms.DataGridViewTextBoxColumn log;
+        private System.Windows.Forms.Button btnAñadirLog;
+        private System.Windows.Forms.Button btnLeerDirectorio;
+        private System.Windows.Forms.TextBox txbLeerDirectorio;
     }
 }
 
--- Ejemplo1/Archivos/Introduccion Archivos/Introduccion Archivos/Form1.cs
+++ Ejemplo1/Archivos/Introduccion Archivos/Introduccion Archivos/Form1.cs
@@ -7,6 +7,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO; 
+
+
 
 namespace Introduccion_Archivos
 {
@@ -19,6 +22,94 @@ namespace Introduccion_Archivos
 
         private void Form1_Load(object sender, EventArgs e)
         {
+
+        }
+
+        private void btnAñadirLog_Click(object sender, EventArgs e)
+        {
+            string log = " Prueba de log ";
+            string tipo = "info ";
+
+            int posicionNuevoLog = dgvLogs.Rows.Add();
+
+
+
+            dgvLogs.Rows[posicionNuevoLog].Cells[0].Value = tipo;
+            dgvLogs.Rows[posicionNuevoLog].Cells[1].Value = log;
+
+
+
+
+            /*   1)  Defirnir la cadena de Texto
+                 2)  llamar al Data griv view 
+                 3)  Añadir Fila 
+                 4)                             
+             */
+
+
+
+        }
+
+        private void dgvLogs_CellDoubleClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+            
+            
+                DataGridViewTextBoxCell celdaTipo = (DataGridViewTextBoxCell)dgvLogs.Rows[e.RowIndex].Cells[0];
+                DataGridViewTextBoxCell celdaLog = (DataGridViewTextBoxCell)dgvLogs.Rows[e.RowIndex].Cells[1];
+
+
+            if (celdaTipo.Value != null)
+            {
+                string tipo = celdaTipo.Value.ToString();
+                string log = celdaLog.Value.ToString();
+
+                
+                // como usar un message box (mensage de Texto 
+                // mensaje 
+                //titulo
+                //Botones
+                //Icono
+
+                MessageBoxIcon icono = MessageBoxIcon.Information;
+                MessageBoxButtons botones = MessageBoxButtons.OK;
+                MessageBox.Show(log, tipo, botones, icono);
+            }
+                       
+         }
+
+        private void btnLeerDirectorio_Click(object sender, EventArgs e)
+        {
+            try // cuando todo esta ok
+            {
+                string path = txbLeerDirectorio.Text;
+
+                // directory 
+
+                DirectoryInfo infoDirectorio = new DirectoryInfo(@path);
+               string fechaDeCreacion = infoDirectorio.CreationTime.ToString();
+                string nombreCompleto = infoDirectorio.FullName.ToString();
+                string directorioPadre =infoDirectorio.Parent.ToString();
+                string directorioRaiz = infoDirectorio.Root.ToString();
+
+                EscribirLog("info", fechaDeCreacion, dgvLogs);
+                EscribirLog("info", nombreCompleto, dgvLogs);
+                EscribirLog("info", directorioPadre, dgvLogs);
+                EscribirLog("info", directorioRaiz, dgvLogs);
+
+            }
+
+            catch (Exception error) // cuando ocurre Error
+            {
+                EscribirLog("error", error.ToString(), dgvLogs);
+            }
+
+            void EscribirLog(string tipo, string log,DataGridView dgv)
+            {
+                int posicionNuevoLog = dgvLogs.Rows.Add();
+                dgvLogs.Rows[posicionNuevoLog].Cells[0].Value = tipo;
+                dgvLogs.Rows[posicionNuevoLog].Cells[1].Value = log;
+            }
 
         }
     }
