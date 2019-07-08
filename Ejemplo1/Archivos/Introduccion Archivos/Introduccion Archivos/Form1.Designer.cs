ad2b458ff73c13b171b61e6ac5cda4b233ed62f1
Gustavo-Contreras <50632467+Gustavo-Contreras@users.noreply.github.com>
2019-07-08 09:45:11 -0500

    avance de introduccion Archivos

--- Ejemplo1/Archivos/Introduccion Archivos/Introduccion Archivos/Form1.Designer.cs
+++ Ejemplo1/Archivos/Introduccion Archivos/Introduccion Archivos/Form1.Designer.cs
@@ -1,4 +1,6 @@
-﻿namespace Introduccion_Archivos
+﻿using System;
+
+namespace Introduccion_Archivos
 {
     partial class Form1
     {
@@ -34,19 +36,30 @@
             this.btnAñadirLog = new System.Windows.Forms.Button();
             this.btnLeerDirectorio = new System.Windows.Forms.Button();
             this.txbLeerDirectorio = new System.Windows.Forms.TextBox();
+            this.btnListarDirectorio = new System.Windows.Forms.Button();
+            this.txbListarDirectorio = new System.Windows.Forms.TextBox();
+            this.txbCrearArchivo = new System.Windows.Forms.TextBox();
+            this.btnCrearDirectorio = new System.Windows.Forms.Button();
+            this.txbCrearDirectorio = new System.Windows.Forms.TextBox();
+            this.btnCrearArchivo = new System.Windows.Forms.Button();
+            this.btnCopiarArchvios = new System.Windows.Forms.Button();
+            this.txbDestino = new System.Windows.Forms.TextBox();
+            this.txbOrigen = new System.Windows.Forms.TextBox();
             ((System.ComponentModel.ISupportInitialize)(this.dgvLogs)).BeginInit();
             this.SuspendLayout();
             // 
             // dgvLogs
             // 
+            this.dgvLogs.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
             this.dgvLogs.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dgvLogs.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
             this.tipo,
             this.log});
-            this.dgvLogs.Location = new System.Drawing.Point(106, 136);
+            this.dgvLogs.Location = new System.Drawing.Point(12, 136);
             this.dgvLogs.Name = "dgvLogs";
-            this.dgvLogs.Size = new System.Drawing.Size(444, 150);
+            this.dgvLogs.Size = new System.Drawing.Size(966, 205);
             this.dgvLogs.TabIndex = 0;
+            this.dgvLogs.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvLogs_CellContentClick);
             this.dgvLogs.CellDoubleClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvLogs_CellDoubleClick);
             // 
             // tipo
@@ -63,9 +76,10 @@
             // 
             // btnAñadirLog
             // 
-            this.btnAñadirLog.Location = new System.Drawing.Point(106, 83);
+            this.btnAñadirLog.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnAñadirLog.Location = new System.Drawing.Point(12, 364);
             this.btnAñadirLog.Name = "btnAñadirLog";
-            this.btnAñadirLog.Size = new System.Drawing.Size(200, 23);
+            this.btnAñadirLog.Size = new System.Drawing.Size(397, 23);
             this.btnAñadirLog.TabIndex = 1;
             this.btnAñadirLog.Text = "Añadir log";
             this.btnAñadirLog.UseVisualStyleBackColor = true;
@@ -73,9 +87,10 @@
             // 
             // btnLeerDirectorio
             // 
-            this.btnLeerDirectorio.Location = new System.Drawing.Point(343, 83);
+            this.btnLeerDirectorio.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnLeerDirectorio.Location = new System.Drawing.Point(12, 90);
             this.btnLeerDirectorio.Name = "btnLeerDirectorio";
-            this.btnLeerDirectorio.Size = new System.Drawing.Size(207, 23);
+            this.btnLeerDirectorio.Size = new System.Drawing.Size(187, 23);
             this.btnLeerDirectorio.TabIndex = 2;
             this.btnLeerDirectorio.Text = "Leer Directorio";
             this.btnLeerDirectorio.UseVisualStyleBackColor = true;
@@ -83,17 +98,108 @@
             // 
             // txbLeerDirectorio
             // 
-            this.txbLeerDirectorio.Location = new System.Drawing.Point(106, 35);
+            this.txbLeerDirectorio.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.txbLeerDirectorio.Location = new System.Drawing.Point(12, 35);
             this.txbLeerDirectorio.Name = "txbLeerDirectorio";
-            this.txbLeerDirectorio.Size = new System.Drawing.Size(444, 20);
+            this.txbLeerDirectorio.Size = new System.Drawing.Size(187, 20);
             this.txbLeerDirectorio.TabIndex = 3;
+            // 
+            // btnListarDirectorio
+            // 
+            this.btnListarDirectorio.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnListarDirectorio.Location = new System.Drawing.Point(216, 90);
+            this.btnListarDirectorio.Name = "btnListarDirectorio";
+            this.btnListarDirectorio.Size = new System.Drawing.Size(150, 23);
+            this.btnListarDirectorio.TabIndex = 4;
+            this.btnListarDirectorio.TabStop = false;
+            this.btnListarDirectorio.Text = "Listar Directorio";
+            this.btnListarDirectorio.UseVisualStyleBackColor = true;
+            this.btnListarDirectorio.Click += new System.EventHandler(this.btnListarDirectorio_Click);
+            // 
+            // txbListarDirectorio
+            // 
+            this.txbListarDirectorio.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.txbListarDirectorio.Location = new System.Drawing.Point(216, 35);
+            this.txbListarDirectorio.Name = "txbListarDirectorio";
+            this.txbListarDirectorio.Size = new System.Drawing.Size(150, 20);
+            this.txbListarDirectorio.TabIndex = 5;
+            this.txbListarDirectorio.TextChanged += new System.EventHandler(this.texbListarDirectorio_TextChanged);
+            // 
+            // txbCrearArchivo
+            // 
+            this.txbCrearArchivo.Location = new System.Drawing.Point(372, 35);
+            this.txbCrearArchivo.Name = "txbCrearArchivo";
+            this.txbCrearArchivo.Size = new System.Drawing.Size(164, 20);
+            this.txbCrearArchivo.TabIndex = 7;
+            this.txbCrearArchivo.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // btnCrearDirectorio
+            // 
+            this.btnCrearDirectorio.Location = new System.Drawing.Point(542, 90);
+            this.btnCrearDirectorio.Name = "btnCrearDirectorio";
+            this.btnCrearDirectorio.Size = new System.Drawing.Size(189, 23);
+            this.btnCrearDirectorio.TabIndex = 8;
+            this.btnCrearDirectorio.Text = "Crear Directorio";
+            this.btnCrearDirectorio.UseVisualStyleBackColor = true;
+            this.btnCrearDirectorio.Click += new System.EventHandler(this.btnCrearDirectorio_Click);
+            // 
+            // txbCrearDirectorio
+            // 
+            this.txbCrearDirectorio.Location = new System.Drawing.Point(542, 35);
+            this.txbCrearDirectorio.Name = "txbCrearDirectorio";
+            this.txbCrearDirectorio.Size = new System.Drawing.Size(189, 20);
+            this.txbCrearDirectorio.TabIndex = 9;
+            this.txbCrearDirectorio.TextChanged += new System.EventHandler(this.txbCrearDirectorio_TextChanged);
+            // 
+            // btnCrearArchivo
+            // 
+            this.btnCrearArchivo.Location = new System.Drawing.Point(372, 90);
+            this.btnCrearArchivo.Name = "btnCrearArchivo";
+            this.btnCrearArchivo.Size = new System.Drawing.Size(164, 23);
+            this.btnCrearArchivo.TabIndex = 10;
+            this.btnCrearArchivo.Text = "Crear Archivos";
+            this.btnCrearArchivo.UseVisualStyleBackColor = true;
+            this.btnCrearArchivo.Click += new System.EventHandler(this.btnCrearArchivo_Click);
+            // 
+            // btnCopiarArchvios
+            // 
+            this.btnCopiarArchvios.Location = new System.Drawing.Point(738, 90);
+            this.btnCopiarArchvios.Name = "btnCopiarArchvios";
+            this.btnCopiarArchvios.Size = new System.Drawing.Size(240, 24);
+            this.btnCopiarArchvios.TabIndex = 11;
+            this.btnCopiarArchvios.Text = "Copiar Archivos";
+            this.btnCopiarArchvios.UseVisualStyleBackColor = true;
+            this.btnCopiarArchvios.Click += new System.EventHandler(this.btnCopiarArchvios_Click);
+            // 
+            // txbDestino
+            // 
+            this.txbDestino.Location = new System.Drawing.Point(861, 34);
+            this.txbDestino.Name = "txbDestino";
+            this.txbDestino.Size = new System.Drawing.Size(117, 20);
+            this.txbDestino.TabIndex = 13;
+            // 
+            // txbOrigen
+            // 
+            this.txbOrigen.Location = new System.Drawing.Point(738, 34);
+            this.txbOrigen.Name = "txbOrigen";
+            this.txbOrigen.Size = new System.Drawing.Size(110, 20);
+            this.txbOrigen.TabIndex = 14;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(990, 450);
+            this.Controls.Add(this.txbOrigen);
+            this.Controls.Add(this.txbDestino);
+            this.Controls.Add(this.btnCopiarArchvios);
+            this.Controls.Add(this.btnCrearArchivo);
+            this.Controls.Add(this.txbCrearDirectorio);
+            this.Controls.Add(this.btnCrearDirectorio);
+            this.Controls.Add(this.txbCrearArchivo);
+            this.Controls.Add(this.txbListarDirectorio);
+            this.Controls.Add(this.btnListarDirectorio);
             this.Controls.Add(this.txbLeerDirectorio);
             this.Controls.Add(this.btnLeerDirectorio);
             this.Controls.Add(this.btnAñadirLog);
@@ -108,6 +214,11 @@
 
         }
 
+        private void texbListarDirectorio_TextChanged(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
         #endregion
 
         private System.Windows.Forms.DataGridView dgvLogs;
@@ -116,6 +227,15 @@
         private System.Windows.Forms.Button btnAñadirLog;
         private System.Windows.Forms.Button btnLeerDirectorio;
         private System.Windows.Forms.TextBox txbLeerDirectorio;
+        private System.Windows.Forms.Button btnListarDirectorio;
+        private System.Windows.Forms.TextBox txbListarDirectorio;
+        private System.Windows.Forms.TextBox txbCrearArchivo;
+        private System.Windows.Forms.Button btnCrearDirectorio;
+        private System.Windows.Forms.TextBox txbCrearDirectorio;
+        private System.Windows.Forms.Button btnCrearArchivo;
+        private System.Windows.Forms.Button btnCopiarArchvios;
+        private System.Windows.Forms.TextBox txbDestino;
+        private System.Windows.Forms.TextBox txbOrigen;
     }
 }
 
--- Ejemplo1/Archivos/Introduccion Archivos/Introduccion Archivos/Form1.cs
+++ Ejemplo1/Archivos/Introduccion Archivos/Introduccion Archivos/Form1.cs
@@ -38,7 +38,7 @@ namespace Introduccion_Archivos
             dgvLogs.Rows[posicionNuevoLog].Cells[1].Value = log;
 
 
-
+           
 
             /*   1)  Defirnir la cadena de Texto
                  2)  llamar al Data griv view 
@@ -64,14 +64,22 @@ namespace Introduccion_Archivos
                 string tipo = celdaTipo.Value.ToString();
                 string log = celdaLog.Value.ToString();
 
-                
+
                 // como usar un message box (mensage de Texto 
                 // mensaje 
                 //titulo
                 //Botones
                 //Icono
+                MessageBoxIcon icono;
 
-                MessageBoxIcon icono = MessageBoxIcon.Information;
+                if(tipo=="error")
+                {
+                    icono = MessageBoxIcon.Error;
+                }
+                else
+                {
+                    icono = MessageBoxIcon.Information;
+                }
                 MessageBoxButtons botones = MessageBoxButtons.OK;
                 MessageBox.Show(log, tipo, botones, icono);
             }
@@ -87,16 +95,31 @@ namespace Introduccion_Archivos
                 // directory 
 
                 DirectoryInfo infoDirectorio = new DirectoryInfo(@path);
-               string fechaDeCreacion = infoDirectorio.CreationTime.ToString();
+
+                
+
+                string fechaDeCreacion = infoDirectorio.CreationTime.ToString();
                 string nombreCompleto = infoDirectorio.FullName.ToString();
                 string directorioPadre =infoDirectorio.Parent.ToString();
                 string directorioRaiz = infoDirectorio.Root.ToString();
 
-                EscribirLog("info", fechaDeCreacion, dgvLogs);
-                EscribirLog("info", nombreCompleto, dgvLogs);
-                EscribirLog("info", directorioPadre, dgvLogs);
-                EscribirLog("info", directorioRaiz, dgvLogs);
+               
 
+                EscribirLog("info","Leyendo Directorio:  " + fechaDeCreacion, dgvLogs);
+                EscribirLog("info", "Leyendo Directorio: " + nombreCompleto, dgvLogs);
+                EscribirLog("info", "Leyendo Directorio: " + directorioPadre, dgvLogs);
+                EscribirLog("info", "Leyendo Directorio: " + directorioRaiz, dgvLogs);
+                FileInfo[] archivosDiectorio = infoDirectorio.GetFiles();
+
+                foreach(FileInfo archivo in archivosDiectorio  )
+                {
+
+                    EscribirLog("info", "Archivos" + archivo.CreationTime, dgvLogs);
+                    EscribirLog("info", "Archivos" + archivo.FullName, dgvLogs);
+                    EscribirLog("info", "Archivos" + archivo.Extension, dgvLogs);
+                    EscribirLog("info", "Archivos" + archivo.LastAccessTime, dgvLogs);
+                    EscribirLog("info", "Archivos" + archivo.IsReadOnly, dgvLogs);
+                }
             }
 
             catch (Exception error) // cuando ocurre Error
@@ -104,13 +127,175 @@ namespace Introduccion_Archivos
                 EscribirLog("error", error.ToString(), dgvLogs);
             }
 
-            void EscribirLog(string tipo, string log,DataGridView dgv)
+            
+
+        }
+        void EscribirLog(string tipo, string log, DataGridView dgv)
+        {
+            int posicionNuevoLog = dgvLogs.Rows.Add();
+            dgvLogs.Rows[posicionNuevoLog].Cells[0].Value = tipo;
+            dgvLogs.Rows[posicionNuevoLog].Cells[1].Value = log;
+
+            if (tipo == "error")
             {
-                int posicionNuevoLog = dgvLogs.Rows.Add();
-                dgvLogs.Rows[posicionNuevoLog].Cells[0].Value = tipo;
-                dgvLogs.Rows[posicionNuevoLog].Cells[1].Value = log;
+                dgvLogs.Rows[posicionNuevoLog].Cells[0].Style.BackColor = Color.Red;
+            }
+            else
+            {
+                dgvLogs.Rows[posicionNuevoLog].Cells[0].Style.BackColor = Color.AliceBlue;
             }
 
+        }
+        private void dgvLogs_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+       void btnListarDirectorio_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string path = txbListarDirectorio.Text; 
+                string[] arregloDirectorios = Directory.GetDirectories(path);
+
+                int numeroDiectorios = arregloDirectorios.Length;
+
+                EscribirLog("info", "Numero de Directorios" + numeroDiectorios.ToString(), dgvLogs);
+
+                foreach(string directorio  in arregloDirectorios )
+                {
+                    EscribirLog("info ", "directorios:  " + directorio, dgvLogs);
+                }
+            }
+            catch(Exception error)
+            {
+                EscribirLog("error", error.ToString(), dgvLogs);
+            }
+           
+        }
+
+        
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txbCrearDirectorio_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnCrearDirectorio_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string path = txbCrearDirectorio.Text;
+                Directory.CreateDirectory(path);
+
+                EscribirLog("info", "crear Directorio: Se creo el directorio  " + path, dgvLogs);
+
+
+            }
+            catch(Exception error)
+            {
+
+                EscribirLog("error", error.ToString(), dgvLogs);
+            }
+        }
+
+      
+        private void btnCrearArchivo_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string path = txbCrearArchivo.Text;
+
+                
+
+
+                StreamWriter escritura = File.CreateText(@path);
+
+
+                escritura.WriteLine("Hola archivo");
+                escritura.WriteLine("Segunda linea");
+                escritura.WriteLine("Tercera linea");
+
+
+                // Siempre cerrar el archivo despues de usarlo
+                escritura.Close();
+
+
+                using (StreamWriter escrituraDos = File.CreateText(@path))
+                {
+                    escrituraDos.WriteLine("Hola archivo");
+                    escrituraDos.WriteLine("Segunda linea");
+                    escrituraDos.WriteLine("Tercera linea");
+                }
+           
+
+                
+
+           EscribirLog("info", "Escritura Archivo: se escribio el archivo", dgvLogs);
+
+            }
+            catch (Exception error)
+            {
+                EscribirLog("error", error.ToString(), dgvLogs);
+            }
+        }
+
+        private void btnCopiarArchvios_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string pathInicio = txbOrigen.Text;
+                string pathFin = txbDestino.Text;
+                char[] cadenaInicio = pathInicio.ToCharArray();
+
+
+                // directory 
+
+                bool existeDirectorioInicio = Directory.Exists(@pathInicio);
+                if (existeDirectorioInicio == true)
+                {
+                    string[] directoriosInicio = Directory.GetDirectories(@pathInicio);
+
+
+                    //ARCHIVOS
+                   
+                    string[] archivosEncontrados = Directory.GetFiles(pathInicio);
+                    //File.Copy()
+
+
+                    foreach (string directorio  in directoriosInicio)
+                    {
+                        EscribirLog("info", directorio, dgvLogs);
+
+                        int inicioSubstring = pathInicio.Length;
+                        int finSubstring = directorio.Length;
+                        string nomreDirectorioi = directorio.Substring(inicioSubstring,finSubstring);
+
+                        EscribirLog("info", directorio, dgvLogs);
+                    }
+
+                }
+                else
+                {
+                    EscribirLog("error", "No existe directorio.", dgvLogs);
+                }
+
+
+
+            }
+            catch(Exception error)
+            {
+                EscribirLog("error", error.ToString(), dgvLogs);
+            }
         }
     }
 }
